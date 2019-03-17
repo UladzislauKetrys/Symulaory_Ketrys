8f62c644a118f3262231ced01b56766e1f889b64
UladzislauKetrys <48184770+UladzislauKetrys@users.noreply.github.com>
2019-03-18 00:56:34 +0100

    first

---
+++ Symulator/Assets/Scripts/echo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class echo : MonoBehaviour {
+
+    public GameObject echosonda;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        System.Random rand = new System.Random();
+        double bl1 = 1.0 - rand.NextDouble();
+        double bl2 = 1.0 - rand.NextDouble();
+        double blad = ((System.Math.Sqrt(-2.0 * System.Math.Log(bl1)) * System.Math.Sin(2.0 * System.Math.PI * bl2)) * 0.3);
+
+        RaycastHit collide;
+        Ray kierunek = new Ray(transform.position, Vector3.down );
+        Debug.DrawRay(transform.position, Vector3.down * 1000);
+
+        if (Physics.Raycast(kierunek, out collide))
+        {
+            if (collide.collider.tag == "Terrain")
+            {
+                double distance = collide.distance + blad;
+                Debug.Log("Blad: " + distance);
+            }
+        }
+
+
+
+
+	}
+}
---
+++ Symulator/Assets/Scripts/nomoto.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nomoto : MonoBehaviour {
+
+    public Rigidbody Ship;
+    public Slider velocity;
+    public Slider course;
+    private Vector3 euler;
+
+    const float recoveryRate = 1000f;
+    float verticalInput = 0.0f;
+    float horizontalInput = 0.0f;
+    const float T = 48.5f;
+    const float K = 0.1232f;
+
+	// Use this for initialization
+	void Start () {
+        Ship = GetComponent<Rigidbody>();
+
+        velocity.onValueChanged.AddListener(Movement);
+        course.onValueChanged.AddListener(Steer);
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        verticalInput = Mathf.MoveTowards(verticalInput, (velocity.value), recoveryRate * Time.deltaTime);
+        horizontalInput = Mathf.MoveTowards(horizontalInput, (course.value), recoveryRate * Time.deltaTime );
+        Movement(velocity.value);
+        Steer(course.value);
+        
+	}
+    void Movement(float maxStrength)
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, verticalInput);
+        Ship.AddRelativeForce(movement);
+    }
+
+    void Steer(float maxStrength)
+    {
+        float r = (K * verticalInput * Time.deltaTime - horizontalInput * (Time.deltaTime - T)) / T;
+        Ship.transform.rotation = Quaternion.Euler(0.0f, r * 180f * Time.deltaTime, 0.0f);
+    }
+}
